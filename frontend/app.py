d18d24c7e33dc4b39171e6bb89d6c9511159bdf9
diogocastros1 <diogoacsantos93@gmail.com>
2024-11-19 23:00:27 -0300

    feat: added new pages and init routes

--- frontend/app.py
+++ frontend/app.py
@@ -9,6 +9,13 @@ API_URL = "http://backend:8000"
 def home():
     return render_template("index.html")
 
+@app.route("/listar")
+def listing():
+    return render_template("listing.html")
+
+@app.route("/cadastro")
+def create():
+    return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="0.0.0.0")
